81229f4453ee52beb449ccc9909ea8b5b4583866
Sodentix <fabianlampert9@gmail.com>
2025-10-23 21:50:10 +0200

    Homepage prototype 01

---
+++ source/frontend/src/components/GamemodeCard.vue
@@ -0,0 +1,85 @@
+<script setup>
+import { Icon } from '@iconify/vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: false,
+    default: ''
+  },
+  icon: {
+    type: String,
+    required: false, 
+  },
+  iconColor: {
+    type: String,
+    required: false,
+    default: 'red'
+  },
+  iconWidth: {
+    type: String,
+    required: false,
+    default: '7rem' // bisherige Breite
+  },
+  iconHeight: {
+    type: String,
+    required: false,
+    default: '7rem' // bisherige Höhe
+  }
+});
+</script>
+
+<template>
+  <div class="card">
+    <div class="imageContent">
+      <Icon
+        :icon="icon"
+        :width="iconWidth"
+        :height="iconHeight"
+        :color="iconColor"
+      />
+    </div>
+    <div class="textContent">
+      <p class="title">{{ title }}</p>
+      <p class="description">{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  background-color: var(--card);
+  height: 14rem;
+  width: 15rem;
+  padding: 1rem;
+  border-radius: 1rem;
+  gap: 0.5rem;
+
+  transition: transform 0.28s ease, box-shadow 0.28s ease;
+}
+
+.title {
+  font-weight: 700;
+  font-size: 1.2rem;
+  color: var(--text);
+}
+
+.description {
+  font-size: 0.95rem;
+  color: var(--text);
+  opacity: 0.8;
+}
+
+.card:hover {
+  transform: translateY(-0.3rem);
+  filter: brightness(2);
+}
+
+</style>
--- source/frontend/src/views/HomeView.vue
+++ source/frontend/src/views/HomeView.vue
@@ -1,5 +1,58 @@
+<script>
+import { defineComponent } from 'vue'
+import GamemodeCard from '@/components/GamemodeCard.vue'
+
+export default defineComponent({
+  components: {
+    GamemodeCard
+  },
+})
+</script>
+
 <template>
-  <div>
-  <h1>This is the homepage</h1>
+  <div class="main">
+  <h1>Kannst du den Song erraten?</h1>
+  <h2>Teste dein Musikwissen. Spiel' gegen die Zeit. Erklimme das Leaderboard.</h2>
+
+  <!-- Parameter sind title, description, icon, iconColor, iconWidth und iconHeight-->
+  <!-- Icons werden je nach Icon-Collection unterschiedlich angegeben - in der Doku nachlesen!-->
+   <div class="gamemodeDiv">
+  <GamemodeCard title="Free Play" description="Teste dein Können ohne Zeitdruck" icon="mdi:music" iconColor="purple"/>
+  <GamemodeCard title="Challenge Mode" description="Spiele gegen die Uhr und erklimme das Leaderboard" icon="mdi:trophy-outline" iconColor="yellow"/>
+  </div>
+
   </div>
 </template>
+
+<style>
+body {
+  background-color: var(--bg);
+}
+
+h1 {
+  background: linear-gradient(90deg, var(--accent-pink), var(--accent-cyan), var(--accent-green));
+  background-size: 1000px; /* Große Breite, damit Gradient über mehrere Texte hinweg gleich bleibt */
+  background-repeat: repeat;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  font-size: 3rem;
+  font-weight: 700;
+}
+
+p, h2 {
+  color: var(--text);
+}
+
+.gamemodeDiv {
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  padding-top: 2rem;
+}
+
+.main {
+  margin-top: 5rem;
+  margin-left: 2.5rem;
+}
+</style>
