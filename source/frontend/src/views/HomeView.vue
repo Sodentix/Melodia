f16ade092ff87804156c65d9f9e26451a14e7c69
Sodentix <fabianlampert9@gmail.com>
2025-10-25 22:33:41 +0200

    Added leaderboard prototype

--- source/frontend/src/views/HomeView.vue
+++ source/frontend/src/views/HomeView.vue
@@ -1,26 +1,34 @@
 <script>
 import { defineComponent } from 'vue'
 import GamemodeCard from '@/components/GamemodeCard.vue'
+import LeaderboardComponent from '@/components/LeaderboardComponent.vue';
 
 export default defineComponent({
   components: {
-    GamemodeCard
+    GamemodeCard,
+    LeaderboardComponent
   },
 })
 </script>
 
 <template>
   <div class="main">
-  <h1>Kannst du den Song erraten?</h1>
-  <h2>Teste dein Musikwissen. Spiel' gegen die Zeit. Erklimme das Leaderboard.</h2>
-
-  <!-- Parameter sind title, description, icon, iconColor, iconWidth und iconHeight-->
-  <!-- Icons werden je nach Icon-Collection unterschiedlich angegeben - in der Doku nachlesen!-->
-   <div class="gamemodeDiv">
-  <GamemodeCard title="Free Play" description="Teste dein Können ohne Zeitdruck" icon="mdi:music" iconColor="purple"/>
-  <GamemodeCard title="Challenge Mode" description="Spiele gegen die Uhr und erklimme das Leaderboard" icon="mdi:trophy-outline" iconColor="yellow"/>
-  </div>
-
+    <div class="topSection">
+      <div class="topLeft">
+        <h1>Kannst du den Song erraten?</h1>
+        <h2>Teste dein Musikwissen. Spiel' gegen die Zeit. Erklimme das Leaderboard.</h2>
+      
+        <div class="gamemodeDiv">
+          <!-- Parameter sind title, description, icon, iconColor, iconWidth und iconHeight-->
+          <!-- Icons werden je nach Icon-Collection unterschiedlich angegeben - in der Doku nachlesen!-->
+          <GamemodeCard title="Free Play" description="Teste dein Können ohne Zeitdruck" icon="mdi:music" iconColor="purple"/>
+          <GamemodeCard title="Challenge Mode" description="Spiele gegen die Uhr und erklimme das Leaderboard" icon="mdi:trophy-outline" iconColor="yellow"/>
+        </div>
+      </div>
+      <div class="leaderboardDiv">
+        <LeaderboardComponent/>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -49,6 +57,19 @@ p, h2 {
   flex-direction: row;
   gap: 1rem;
   padding-top: 2rem;
+}
+
+.leaderboardDiv {
+  padding-right: 5rem;
+}
+
+.topSection {
+  display: flex;
+  flex-direction: row;
+}
+
+.topLeft {
+  flex-direction: column;
 }
 
 .main {
